22b097ece9306b509bf9342b7ad7d8227a35e1b9
Alix Barreaux <alixbarreaux@tutanota.com>
2021-01-02 05:31:35 +0100

    update the translations

---
+++ GUI/Buttons/quit_to_desktop_button.gd
@@ -0,0 +1,14 @@
+extends Button
+
+# -------------------- DECLARE VARIABLES --------------------
+
+# --------------------  DECLARE SIGNALS  --------------------
+
+# --------------------   RUN THE CODE    --------------------
+
+
+# -------------------- DECLARE FUNCTIONS --------------------
+
+
+func _on_QuitToDesktopButton_pressed() -> void:
+	get_tree().quit()
