9e5a2ee366e28f683424f21e6c5347dd3a545931
Alix Barreaux <alixbarreaux@tutanota.com>
2021-01-06 10:42:47 +0100

    continue on level designing

--- Interactables/Furnitures/Doors/Base/door_base.gd
+++ Interactables/Furnitures/Doors/Base/door_base.gd
@@ -70,9 +70,11 @@ func check_if_player_has_key() -> void:
 
 func open() -> void:
 	model.animation_player.play(model.animation_list[0])
+	$AudioStreamPlayer3D.play()
 	WorldEvents.door_closed_amount -= 1
 
 
 func close() -> void:
 	model.animation_player.play_backwards(model.animation_list[0])
+	$AudioStreamPlayer3D.play()
 	WorldEvents.door_closed_amount += 1
